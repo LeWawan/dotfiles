65700627fd9adbb2a23e3afb000eef16eeaac3b7
LeWawan <erwankreutz@gmail.com>
2022-11-06 18:19:43 +0100

    🐛 fix lua lsp globals

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -107,7 +107,7 @@ lspconfig.sumneko_lua.setup(config({
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
+        globals = { 'vim', 'require', 'os', 'package' },
       },
       workspace = {
         -- library = vim.api.nvim_get_runtime_file('', true)
