89ffa6edf73e3e0597556682c5d597d0700bb29b
erwan <erwan@hellotrusty.io>
2023-04-23 15:05:33 +0200

    feat: update tokyonight conf

--- nvim/.config/nvim/after/plugin/tokyonight.lua
+++ nvim/.config/nvim/after/plugin/tokyonight.lua
@@ -3,7 +3,7 @@ vim.opt.background = 'dark'
 require("tokyonight").setup({
   -- your configuration comes here
   -- or leave it empty to use the default settings
-  style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
   transparent = true, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
   styles = {
@@ -38,4 +38,48 @@ require("tokyonight").setup({
 vim.cmd("let g:lightline = {'colorscheme': 'tokyonight', 'active': {'left': [[ 'mode', 'paste', 'lsp_info', 'lsp_hints', 'lsp_errors', 'lsp_warnings', 'lsp_ok' ],[ 'gitbranch', 'readonly', 'relativepath', 'lsp_status', 'modified' ] ]}, 'component_function': { 'gitbranch': 'FugitiveHead' }} ")
 vim.cmd("call lightline#lsp#register()")
 
-vim.cmd('colorscheme tokyonight')
+vim.cmd('colorscheme tokyonight-storm')
+
+
+
+
+
+
+-- require('everblush').setup({
+--
+--     -- Default options
+--     override = {},
+--
+--     -- Set transparent background
+--     transparent_background = false,
+--     -- transparent_background = true,
+--
+--     -- Set contrast for nvim-tree highlights
+--     nvim_tree = {
+--         contrast = false,
+--     },
+--     -- nvim_tree = {
+--     --     contrast = true,
+--     -- },
+--
+--
+--
+--   styles = {
+--     -- Style to be applied to different syntax groups
+--     -- Value is any valid attr-list value for `:help nvim_set_hl`
+--     comments = { italic = true },
+--     keywords = { italic = true },
+--     functions = { italic = true },
+--     variables = {},
+--     -- Background styles. Can be "dark", "transparent" or "normal"
+--     sidebars = "transparent", -- style for sidebars, see below
+--     floats = "dark", -- style for floating windows
+--   },
+--
+-- })
+--
+-- vim.cmd("let g:lightline = {'colorscheme': 'everblush', 'active': {'left': [[ 'mode', 'paste', 'lsp_info', 'lsp_hints', 'lsp_errors', 'lsp_warnings', 'lsp_ok' ],[ 'gitbranch', 'readonly', 'relativepath', 'lsp_status', 'modified' ] ]}, 'component_function': { 'gitbranch': 'FugitiveHead' }} ")
+-- vim.cmd("call lightline#lsp#register()")
+-- vim.cmd('colorscheme everblush')
+
+
