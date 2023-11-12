2bb3b9914d4644b2fb6d3c46b75a272e34cb514b
LeWawan <erwankreutz@gmail.com>
2023-11-12 16:35:45 +0100

    feat: add tabtabl resource for zsh

--- nvim/.config/tabtab/zsh/__tabtab.zsh
+++ nvim/.config/tabtab/zsh/__tabtab.zsh
@@ -2,3 +2,7 @@
 # tabtab source for pnpm package
 # uninstall by removing these lines
 [[ -f ~/.config/tabtab/zsh/pnpm.zsh ]] && . ~/.config/tabtab/zsh/pnpm.zsh || true
+
+# tabtab source for hh package
+# uninstall by removing these lines
+[[ -f ~/.config/tabtab/zsh/hh.zsh ]] && . ~/.config/tabtab/zsh/hh.zsh || true
