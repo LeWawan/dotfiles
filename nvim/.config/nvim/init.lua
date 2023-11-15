2143e6360a21f9e15980072e05a3f8a645a80ca3
erwan <erwan.kreutz@delupay.com>
2023-11-15 22:56:22 +0100

    feat: switch "default" env from "default" to "lazygit"

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,2 @@
-require('thewawan')
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ nvim/.config/nvim/lua/plugins/internal-coding.lua
@@ -0,0 +1,5 @@
+return {
+  -- https://github.com/echasnovski/mini.pairs
+  -- Auto pairs plugin
+  { "echasnovski/mini.pairs", enabled = false },
+}
