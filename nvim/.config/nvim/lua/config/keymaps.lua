d00a35adb41eb46366d2234d060d8d3fe24243ad
LeWawan <erwankreutz@gmail.com>
2023-11-17 23:24:12 +0100

    feat: add discipline script to prevent key mashing

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,9 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
+local discipline = require("lewawan.discipline")
+discipline.cowboy()
+
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
