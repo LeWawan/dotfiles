14b42c0e9d65d6faf8ed8d44f81f0ae9b1f2e1a4
LeWawan <erwankreutz@gmail.com>
2023-11-19 11:36:41 +0100

    refactor: update plugin organization

---
+++ nvim/.config/nvim/tests/typescript.ts
@@ -0,0 +1,11 @@
+type User = {
+  name: string;
+  age: number;
+};
+
+const user: User = {
+  name: "John",
+  age: 30,
+};
+
+user.name = "Jane";
---
+++ nvim/.config/nvim/tests/vue.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const Button = defineComponent({
+  name: "Button",
+  setup() {
+    return () => <button class="button"></button>;
+  },
+});
+</script>
+
+<template>
+  <div>
+    <Button
+      id="button"
+      class="button"
+      @click="(e) => void"
+      toto="tata"
+      tata="tutu"
+      riri="raa"
+      rtou="asdf"
+      asdfasd="asdfasdfasdfsa"
+    >
+    </Button>
+  </div>
+</template>
