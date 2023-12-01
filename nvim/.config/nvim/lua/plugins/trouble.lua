9766cd00e92dd50c41bdf160fef86116785d4953
erwan <erwan.kreutz@delupay.com>
2023-12-01 09:13:01 +0100

    feat: update trouble.nvim keymaps

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -5,8 +5,18 @@ return {
     keys = {
       -- Add all my keymaps
       { "<C-q>", "<cmd>TroubleToggle<cr>" },
-      { "<C-n>", "<cmd>TroubleToggle document_diagnostics<cr>" },
-      { "<C-p>", "<cmd>TroubleToggle document_diagnostics<cr>" },
+      {
+        "<C-n>",
+        function()
+          require("trouble").next({ skip_groups = true, jump = true })
+        end,
+      },
+      {
+        "<C-p>",
+        function()
+          require("trouble").previous({ skip_groups = true, jump = true })
+        end,
+      },
       { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
       { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>" },
       { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>" },
