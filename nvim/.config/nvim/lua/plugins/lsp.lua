29fe41f42ca1aac1450133c0b7f3b020ed5e9d8b
erwan <erwan.kreutz@delupay.com>
2023-12-24 09:43:06 +0100

    refactor: remove ts-server and configure volar to replace it

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        volar = {
+          filetypes = { "vue", "javascript", "typescript", "typescriptreact" },
+        },
+      },
+    },
+  },
+}
