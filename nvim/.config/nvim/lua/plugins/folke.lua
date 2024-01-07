6cdae024f896582458f176598f451f85401a4e3e
wawan <erwankreutz@gmail.com>
2024-01-07 16:29:47 +0100

    feat: add folke/zen-mode

---
+++ nvim/.config/nvim/lua/plugins/folke.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/zen-mode.nvim",
+  config = function()
+    require("zen-mode").setup({
+      window = {
+        width = 120,
+        options = {
+          number = true,
+          relativenumber = true,
+        },
+      },
+    })
+
+    vim.keymap.set("n", "<leader>zz", function()
+      require("zen-mode").toggle()
+      vim.wo.wrap = false
+    end)
+  end,
+}
