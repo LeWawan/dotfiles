ca462864e5ffcdcb1cb7fbc66e1135ef87b491a5
wawan <erwankreutz@gmail.com>
2024-01-07 16:29:33 +0100

    feat: disable some plugins for now

---
+++ nvim/.config/nvim/lua/plugins/neogen.lua
@@ -0,0 +1,19 @@
+-- @TODO: Maybe later...
+-- stylua: ignore
+if true then return {} end
+
+return {
+  {
+    "danymat/neogen",
+    keys = {
+      {
+        "<leader>cc",
+        function()
+          require("neogen").generate({})
+        end,
+        desc = "Neogen Comment",
+      },
+    },
+    opts = { snippet_engine = "luasnip" },
+  },
+}
