4aae60de8bc8bf637d8df6fa02386138419c66ad
LeWawan <erwankreutz@gmail.com>
2024-01-10 12:19:38 +0100

    feat(conform.nvim): add prettier lint

---
+++ nvim/.config/nvim/lua/thewawan/lazy/lint.lua
@@ -0,0 +1,16 @@
+return {
+  'stevearc/conform.nvim',
+  config = function()
+    require('conform').setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        vue = { "prettier" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    })
+  end
+}
