15970f16ccdaf053b4768b0c8c488e7d009f1023
LeWawan <erwankreutz@gmail.com>
2024-01-10 12:00:23 +0100

    feat(telescope): add "open find_grep" with trouble

--- nvim/.config/nvim/lua/thewawan/lazy/telescope.lua
+++ nvim/.config/nvim/lua/thewawan/lazy/telescope.lua
@@ -6,6 +6,8 @@ return {
     },
     config = function()
       local telescope = require('telescope.builtin')
+      local trouble = require("trouble.providers.telescope")
+
       vim.keymap.set('n', "<leader>fp", function() telescope.git_files() end)
       vim.keymap.set('n', "<leader>ff", function() telescope.find_files({ hidden = true }) end)
       vim.keymap.set('n', "<leader>fg", function() telescope.live_grep({ hidden = true }) end)
@@ -15,8 +17,23 @@ return {
       vim.keymap.set('n', "<leader>fh", function() telescope.help_tags() end)
       vim.keymap.set('n', "<leader>gb", function() telescope.git_branches() end)
       vim.keymap.set('n', "<leader>'",
-        function() telescope.find_files({ prompt_title = "< VimRC >", cwd = "~/.dotfiles/", hidden = true }) end)
-      vim.keymap.set('n', "<leader>vh", function() telescope.help_tags() end)
+        function() telescope.builtin.find_files({ prompt_title = "< VimRC >", cwd = "~/.dotfiles/", hidden = true }) end)
+      vim.keymap.set('n', "<leader>vh", function() telescope.builtin.help_tags() end)
+
+      require('telescope').setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-q>"] = trouble.open_with_trouble,
+              ["<C-t>"] = trouble.open_with_trouble,
+            },
+            n = {
+              ["<C-q>"] = trouble.open_with_trouble,
+              ["<C-t>"] = trouble.open_with_trouble,
+            },
+          },
+        }
+      })
     end
   },
 
