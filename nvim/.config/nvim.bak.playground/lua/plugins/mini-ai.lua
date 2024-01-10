b7e4ca8f3e39a7220b9f6ba4ac2b2f7400f7fc05
erwan <erwan.kreutz@delupay.com>
2024-01-10 11:58:45 +0100

    feat: delete old vim conf files

--- nvim/.config/nvim.bak.playground/lua/plugins/mini-ai.lua
+++
@@ -1,5 +0,0 @@
-return {
-  -- https://github.com/echasnovski/mini.ai
-  -- Better tag selection but broken for now @TODO test me later
-  { "echasnovski/mini.ai", enabled = false },
-}
--- nvim/.config/nvim.bak.playground/lua/plugins/mini-pairs.lua
+++
@@ -1,5 +0,0 @@
-return {
-  -- https://github.com/echasnovski/mini.pairs
-  -- Auto pairs plugin
-  { "echasnovski/mini.pairs", enabled = false },
-}
