fb4ca7571ee3c596e40ffa24e46c872ae80da384
LeWawan <erwankreutz@gmail.com>
2025-03-20 22:57:05 +0100

    feat: add dmmulroy/ts-error-translator.nvim

--- nvim/.config/nvim/lua/plugins/lint.lua
+++ nvim/.config/nvim/lua/plugins/lint.lua
@@ -1,17 +1,22 @@
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				vue = { "prettier" },
-				lua = { "stylua" },
-			},
-			format_on_save = {
-				timeout_ms = 1000,
-				lsp_fallback = "fallback",
-			},
-		})
-	end,
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          javascript = { "prettier" },
+          typescript = { "prettier" },
+          vue = { "prettier" },
+          lua = { "stylua" },
+        },
+        format_on_save = {
+          timeout_ms = 1000,
+          lsp_fallback = "fallback",
+        },
+      })
+    end,
+  },
+  {
+    "dmmulroy/ts-error-translator.nvim"
+  }
 }
